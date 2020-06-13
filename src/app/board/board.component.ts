f374670788579733a1cfb55d6785d2492ce16e60
Debadree Chatterjee <debadree333@gmail.com>
2020-06-13 18:24:34 +0530

    created components and basic functionality

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  player: 'X'|'O';
+  winner: string;
+  constructor() {
+    this.beginGame();
+  }
+
+  ngOnInit(): void {
+  }
+
+  beginGame(): void {
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.player = 'X';
+  }
+
+  get currentPlayer() {
+    return this.player;
+  }
+
+  move(ind: number) {
+    if (!this.squares[ind]) {
+      this.squares[ind] = this.currentPlayer;
+      this.player = (this.player === 'X') ? 'O' : 'X';
+    }
+    this.winner = this.getWinner();
+  }
+
+  getWinner(): string {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent implements OnInit {
+  @Input() move: 'X' | 'O' | null;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
